ea80e52efdf3d2c6e6e552c13015dd5ffd789ce8
Helber F Rodrigues <helber.frodrigues@gmail.com>
2024-04-21 19:34:40 -0300

    Implentando função na activity.

--- app/src/main/java/com/example/primeiroandroid/MainActivity.java
+++ app/src/main/java/com/example/primeiroandroid/MainActivity.java
@@ -1,6 +1,18 @@
 package com.example.primeiroandroid;
 
+import static android.app.ProgressDialog.show;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this, "Helber Fernandes Rodrigues",Toast.LENGTH_LONG).show();
+    }
 }
