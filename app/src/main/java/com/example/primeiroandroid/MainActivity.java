7f1064d70fd33668f75073c48804cc3af7a4363e
Helber F Rodrigues <helber.frodrigues@gmail.com>
2024-04-21 20:33:45 -0300

    Implentando layout na activity.

--- app/src/main/java/com/example/primeiroandroid/MainActivity.java
+++ app/src/main/java/com/example/primeiroandroid/MainActivity.java
@@ -5,6 +5,7 @@ import static android.app.ProgressDialog.show;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -13,6 +14,9 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "Helber Fernandes Rodrigues",Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, "Helber Fernandes Rodrigues",Toast.LENGTH_LONG).show();
+        //TextView aluno = new TextView(this);
+        //aluno.setText("Helber Fernandes Rodrigues");
+        setContentView(R.layout.activity_main);
     }
 }
